0de42bfdecb909d6d6c33c5cd47ac96ed5cdd1a7
Nuwana Thilakasiri <nuwana24@gmail.com>
2020-10-07 19:31:53 +0530

    register21

--- backend/Routes/User.routes.js
+++ backend/Routes/User.routes.js
@@ -117,7 +117,8 @@ router.post("/login", (req, res) => {
                     (err, token) => {
                         res.json({
                             success: true,
-                            token: "Bearer " + token
+                            token: "Bearer " + token,
+                            id:payload.id,
                         });
                     }
                 );
